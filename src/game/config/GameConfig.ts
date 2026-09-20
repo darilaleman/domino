cd548ccf8d66d6d5b55ba9165254e1a8d1b27b9b
Daril Alemán Morales <daril.aleman@gmail.com>
2026-09-20 01:01:15 -0400

    3rd fit in screen

--- src/game/config/GameConfig.ts
+++ src/game/config/GameConfig.ts
@@ -1,5 +1,12 @@
 export const GAME_INFO = {
     id: 'cubaplay-domino',
     name: 'Dominó Cubano',
-    version: '1.0.0'
+    version: '1.0.0',
+    scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    parent: 'game-container'
+}
 };
